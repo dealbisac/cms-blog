19cb5a66a341b5c56c3845e10e759e52b855eecd
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-01-18 22:36:12 +0545

    feat: complete set-up code

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Categories = () => {
     return (
         <div>
-            
+            <h1>Categories</h1>
         </div>
     )
 }
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PostWidget = () => {
     return (
         <div>
-            
+            <h3>PostWidget</h3>
         </div>
     )
 }

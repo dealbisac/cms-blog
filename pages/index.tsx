acccdaba3aec9f905a08eb334940c0ac169f7be7
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-01-18 13:34:14 +0545

    refactor: refactor code

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-       {posts.map((post, index) => (
-         <div>
-           {post.title}
-           {post.excerpt}
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+        </div>
+         <div className='lg:col-span-4 col-span-1'>
+           <div className='lg:sticky relative top-8'>
+             <PostWidget />
+             <Categories />
+           </div>
          </div>
-       ))}
       </div>
      
     </div>
